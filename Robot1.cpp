b1072edd833a1faa690fd331d0a69e90e7fcb26c
Gevor94 <gevor.zoroglyan@gmail.com>
2015-12-22 22:34:01 +0400

    Update Robot1.cpp

--- Robot1.cpp
+++ Robot1.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
-using namespace std;
-class board{
-  char str[10][10];
-public:
-	void obstacles() ; //Функция заполняющая препятствиями
-	int treasures(int t) ; //Фун-ия заполняющая сокровищами
-	void walls(); //Фун-ия границ лабиринта
-};
+#include <algorithm>
+using namespace std; 
 
+class cell{
+};                                                                      
+															
+class board
+{
+private:
+	char str[10][10];
+public:
+	int a,b;
+	char *operator[](int i) { return str[i] ;}
+	void obstacles() ; // The function fills obstacles
+	int treasures(int t) ; //The function fills rand treasures 
+	void walls(); //The function of border maze
+	void robot_place(int x , int y);//Robot's place
+
+};
 void board::walls()
 {
 	for(int j = 0 ; j < 10 ; j++)
@@ -39,22 +49,79 @@ void board::walls()
 	}
 
 
-}	
+}
 
 int board::treasures( int t) 
 {
-	int count = t ;
-	
-	int count = 10; 
-	while(count != 0)
+		int count = t ;
+		int count = 10; 
+		while(count != 0)
 	 {
 		int x = rand()% 8 + 1;
 		int y = rand()% 8 + 1;
-		while( str[x][y] != '*' && str[x][y] != '&' && str[x][y] !=' ') // <= здесь есть недоработка в последнем str[x][y] 											//вместо пустых кавычек
-		 {                                                              //будет значение obstacle
+		while( str[x][y] != '*' && str[x][y] != '&' && str[x][y] !='1') 
+		 {
 				str[x][y] = '*';
 				count--;
 		 }
 	 }
 	
+}
+void board::robot_place(int x ,int y)
+{
+	if(str[x][y] != '*')
+	{
+		str[x][y] = '&';
+		a = x;
+		b = y;
+	}
+	else
+	{
+		cout << "The place is already busy ";
+		return;
+	}
+	
+}
+
+void board::obstacles()
+{
+	cout << " Enter '1' for obstacles and '0' for empty cells ";
+	for ( int  i = 1 ; i < 9 ; i++)
+	{
+		for ( int  j = 1 ; j < 9 ; j++)
+		{
+			if(str[i][j] != '*')
+			{
+			cin >> str[i][j];
+			}
+
+			else continue;
+		}
+	}
+	
+}
+
+
+class robot{
+private:
+	char *s;
+	board q;
+public:
+	void ScanLevel();// The function scan board and show answer;
+
+};
+
+void robot::ScanLevel()
+{
+	s = &q[q.a][q.b];
+
+
+
+}
+	
+
+
+int main()
+{
+	return 0;
 }
